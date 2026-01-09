ecbe070072e9e160d2908cc3bfa0a8831b48e1a1
EvilDron09 <andrej32118@gmail.com>
2026-01-09 20:23:07 +0300

    hw1 and lesson1 are done

---
+++ classwork/lesson1/lesson_1-1_script.js
@@ -0,0 +1,27 @@
+console.log(3)
+
+// var a;
+// a='hello okten';
+// {
+//     let a = 'hello okten';
+//     var b = 'hello world';
+// }
+// console.log(a);
+// console.log(b);
+
+// const PI =3.14;
+// console.log(PI);
+//
+// let a = 'fjkhhfejhwfjewi';
+// console.log(a);
+// a='okten';
+// console.log(a);
+
+let a ='hello okten';
+console.log(typeof a, a);
+
+let b = 100500;
+console.log(typeof b, b);
+
+let c = true;
+console.log(typeof c, c)
---
+++ homework/hw1/hw_1_script.js
@@ -0,0 +1,80 @@
+// #dYQNrBV
+// – Створити змінні. Присвоїти кожному з них значення: ‘hello’,’owu’,’com’, ‘ua’, 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//   Вивести кожну змінну за допомогою: console.log
+
+let w = 'hello'
+console.log(w)
+
+let v = 'own'
+console.log(v)
+
+let t = "com"
+console.log(t)
+
+let d = 'ua'
+console.log(d)
+
+let e = 1
+console.log(e)
+
+let f = 10
+console.log(f)
+
+let n = -999
+console.log(n)
+
+let g = 123
+console.log(g)
+
+let j = 3.14
+console.log(j)
+
+let q = 2.7
+console.log(q)
+
+let h = 16
+console.log(h)
+
+let y =true
+console.log(y)
+
+let r = false
+console.log(r)
+
+
+// #6Qb97gsv
+// – Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З’єднати їх в одну змінну person (Не об’єкт, просто за допомогою конкатенації)
+
+let firstName = 'Андрій'
+let middleName = 'Олександрович'
+let lastName = 'Антоненко'
+
+let person = `${lastName} ${firstName} ${middleName}`
+console.log(person)
+
+// #4N0y5tufA
+// – За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = ‘100’; let c = true;
+
+let a =100
+console.log(typeof a)
+
+let b = '100'
+console.log(typeof b)
+
+let c =true
+console.log(typeof c)
+
+// #ruUtWDUI
+// Додаткове для тих, хто цікавився prompt`oм
+// – За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
+
+let yourFirstName = prompt('Your name:')
+console.log(yourFirstName)
+
+let yourMiddleName = prompt('Your middle name:')
+console.log(yourMiddleName)
+
+let yourAge = prompt('Your age:')
+console.log(yourAge)
+
