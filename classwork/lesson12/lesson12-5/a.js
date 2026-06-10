0edf056790e313ac48c600e1cc376d66d644bcc3
EvilDron09 <andrej32118@gmail.com>
2026-06-10 22:00:43 +0300

    lesson 12.5

---
+++ classwork/lesson12/lesson12-5/a.js
@@ -0,0 +1,7 @@
+function asd(){console.log('asd')}
+
+function qwe(){console.log('qwe')}
+
+function zxc(){console.log('zxc')}
+
+export const exportObject = {xxx:asd, zxc};
---
+++ classwork/lesson12/lesson12-5/b.js
@@ -0,0 +1,3 @@
+export function foo(){console.log('foo')}
+
+export  default function bar (){console.log('bar')}
